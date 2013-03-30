f3300155c8ba2f1eb70a5ecb9295b0995cdc63ac
sirkarpfen <stephan.scholz@gmx.at>
2013-03-30 23:45:41 +0200

    removed solo character sprites. spritesheet is used instead

--- SimpleRPG/src/com/github/sirkarpfen/exceptions/IllegalSpawnException.java
+++ SimpleRPG/src/com/github/sirkarpfen/exceptions/IllegalSpawnException.java
@@ -1,5 +1,12 @@
 package com.github.sirkarpfen.exceptions;
 
+/**
+ * Thrown when no spawn was detected by the player. This only happens, if
+ * a map has no object with the name: "spawn" declared.
+ * 
+ * @author sirkarpfen
+ *
+ */
 public class IllegalSpawnException extends Exception {
 
 	private static final long serialVersionUID = 1L;
